3f83b049087270cc62034beecbfc402c16617b95
William Dam <damw@oregonstate.edu>
2021-01-03 22:29:12 -0800

    Initial Commit

---
+++ Image Picker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Image Picker
+//
+//  Created by william dam on 1/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
